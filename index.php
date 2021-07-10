15b93ef66b0a871f825660cd3422968f854e7f2a
Rajitha-19 <72659413+Rajitha-19@users.noreply.github.com>
2021-07-10 23:40:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration Page</title>
+	<link href="styles.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+	<h1>Register Here</h1>
+	<form action="script.php" method="POST" >
+            <div class="formclass">
+                <input class="firstname" id="name" type="text" name="name" required placeholder="UserName"><br>
+				<input class="password" name="pwd" type="password" required placeholder="??????">  
+                <input  type="email" id="email" name="email" required placeholder="Email"><br>
+                <input type="text" id="phno" name="PhoneNumber" required placeholder="Phone Number"><br>
+            </div>
+            
+            <button class="button" type="submit" name="submit" value="submit">Submit</button>
+            <button class="button" type="reset"  value="reset">Reset</button>
+    </form>
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,47 @@
+<?php
+    $servername = "localhost";
+    $username   = "root";
+    $password   = "";
+    $database="userdetails";
+
+    // checking the database connection
+
+    $conn = mysqli_connect($servername,$username,$password,$database);
+    if(!$conn)
+        die("Connection failed: " . mysqli_connect_error());
+    else   
+        echo "Successfully Connected";
+        echo "<br>";
+
+    if($_SERVER['REQUEST_METHOD']=="POST") {
+        $name=$_POST['name'];
+        $pwd=$_POST['pwd'];
+        $email=$_POST['email'];
+        $PhoneNumber=$_POST['PhoneNumber'];
+
+        $sql="INSERT INTO `users` (`name`,`pwd`,`email`,`PhoneNumber`) VALUES ('$name','$pwd','$email','$PhoneNumber')";
+
+        $query=mysqli_query($conn,$sql);
+
+        // if the query is inserted successfully.
+
+        if($query){
+            echo "Successfully inserted user information..";
+            echo "<br><br>";
+            //printing the details of user
+            echo "The user details are :";
+            echo "<br><br>";
+            echo "Username = $name";
+            echo "<br>";
+            echo "Password = $pwd";
+            echo "<br>";
+            echo "Email= $email";
+            echo "<br>";
+            echo "PhoneNumber= $PhoneNumber";
+            echo "<br>";
+        }
+        else{
+            echo "oops! Error Occurred";
+        }
+    }
+?>
